fc1d5ebf8c68de3f1eff4b6f0c75d1c711f73536
Jeongho Jeong <fiil12@hotmail.com>
2017-03-04 19:38:11 +0900

    Setup basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 const path = require('path')
 
 module.exports = {
-  entry: '',
+  entry: './client/index.jsx',
   output: {
-
+    path: path.resolve('./dist'),
+    filename: 'bundle.js'
   },
   resolve: {
-
+    modules: ['node_modules', process.env.NODE_PATH]
+  },
+  resolveLoader: {
+    modules: ['node_modules', process.env.NODE_PATH]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      }
+    ]
   }
 }
